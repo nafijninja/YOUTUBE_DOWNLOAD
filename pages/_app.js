cabba5067d39617335d547bb755c2163948eed86
NAFIJ <168545572+nafijninja@users.noreply.github.com>
2025-04-23 15:16:37 +0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-// pages/_app.js
 import '../styles/globals.css';
 import Head from 'next/head';
 
@@ -8,7 +7,24 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <title>YouTube Downloader</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Download YouTube videos and audio easily." />
+        <meta name="keywords" content="YouTube, downloader, video, audio, download" />
+        
+        {/* Open Graph meta tags for Facebook and LinkedIn */}
+        <meta property="og:title" content="YouTube Downloader" />
+        <meta property="og:description" content="Download YouTube videos and audio with ease." />
+        <meta property="og:image" content="/og-image.jpg" />
+        
+        {/* Twitter meta tags for Twitter Cards */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="YouTube Downloader" />
+        <meta name="twitter:description" content="Download YouTube videos and audio with ease." />
+        <meta name="twitter:image" content="/twitter-image.jpg" />
+        
+        {/* Manifest and Icons */}
         <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </Head>
       <Component {...pageProps} />
     </>
